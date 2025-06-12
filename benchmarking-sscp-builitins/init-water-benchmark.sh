403e1abfbb2382f4f3ab53130b428990b2a4bd9e
Soproni Balint <balint@streamhpc.com>
2025-06-12 23:12:28 +0000

    Also initialize largest water

--- benchmarking-sscp-builitins/init-water-benchmark.sh
+++ benchmarking-sscp-builitins/init-water-benchmark.sh
@@ -9,7 +9,7 @@ tar xvf grappa-1.5k-6.1M_rc0.9.tar.gz\?download\=1
 cd grappa-1.5k-6.1M_rc0.9
 
 
-for problem in 0000.96  0001.5  0003  0006  0012  0024  0048  0096  0192  0384  0768  1536  3072
+for problem in 0000.96  0001.5  0003  0006  0012  0024  0048  0096  0192  0384  0768  1536  3072 6144
 do
   $GMX_BIN_DIR/gmx grompp -f $problem/../pme.mdp -c $problem/conf.gro -r $problem/conf.gro -p $problem/topol.top -o $problem/water.tpr
 done
