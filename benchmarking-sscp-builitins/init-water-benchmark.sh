90630754dbd0475bec72fa8980532801dbc59b2f
sbalint98 <sbsbalint14@gmail.com>
2025-06-13 01:01:36 +0200

    Finalize Docker container for benchmarking

--- benchmarking-sscp-builitins/init-water-benchmark.sh
+++ benchmarking-sscp-builitins/init-water-benchmark.sh
@@ -2,14 +2,15 @@
 source environment.sh
 
 ROOT_DIR=`pwd`
-GMX_BIN_DIR=/gromacs/build/bin/
+GMX_BIN_DIR=/gromacs-sscp-base/build/bin/
 
-wget https://ftp.gromacs.org/benchmarks/water_bare_hbonds.tar.gz
-tar xvf water_bare_hbonds.tar.gz
-cd water-cut1.0_bare_hbonds
+wget https://zenodo.org/records/11234002/files/grappa-1.5k-6.1M_rc0.9.tar.gz?download=1
+tar xvf grappa-1.5k-6.1M_rc0.9.tar.gz\?download\=1
+cd grappa-1.5k-6.1M_rc0.9
+
 
 for problem in 0000.96  0001.5  0003  0006  0012  0024  0048  0096  0192  0384  0768  1536  3072
 do
-  $GMX_BIN_DIR/gmx grompp -f $problem/pme.mdp -c $problem/conf.gro -r $problem/conf.gro -p $problem/topol.top -o $problem/water.tpr
+  $GMX_BIN_DIR/gmx grompp -f $problem/../pme.mdp -c $problem/conf.gro -r $problem/conf.gro -p $problem/topol.top -o $problem/water.tpr
 done
 
